9947b8b9561c8a28268dc34ca5f2205b2eebac26
Charzillion108 <daxtonmalachi@gmail.com>
2026-02-24 14:57:02 -0600

    Update PlayerMovement.cs

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,62 +1,76 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController controller;
     private Vector2 inputVector;
+    private Vector2 lookInput;
     private Vector3 velocity;
     private bool isSprinting = false;
+    private float xRotation = 0f;
 
-    // By using [SerializeField], these stay private but show up in the Inspector
-    [Header("Speeds")]
+    [Header("Movement Speeds")]
     [SerializeField] private float walkSpeed = 5f;
     [SerializeField] private float sprintSpeed = 10f;
 
+    [Header("Look Settings")]
+    [SerializeField] private float mouseSensitivity = 20f;
+    [SerializeField] private Transform cameraTransform;
+
     [Header("Physics")]
     [SerializeField] private float jumpHeight = 2f;
-    [SerializeField] private float gravity = -15f; // Buffed gravity for better feel
+    [SerializeField] private float gravity = -15f;
 
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+        
+        // Lock the cursor for the camera look
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        inputVector = context.ReadValue<Vector2>();
-    }
-
-    public void OnSprint(InputAction.CallbackContext context)
-    {
-        if (context.started || context.performed)
-            isSprinting = true;
-        else if (context.canceled)
-            isSprinting = false;
-    }
-
+    // --- INPUT SYSTEM EVENTS ---
+    public void OnMove(InputAction.CallbackContext context) => inputVector = context.ReadValue<Vector2>();
+    public void OnLook(InputAction.CallbackContext context) => lookInput = context.ReadValue<Vector2>();
     public void OnJump(InputAction.CallbackContext context)
     {
         if (context.performed && controller.isGrounded)
         {
-            // The math for a perfect jump arc
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
+    }
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        if (context.started || context.performed) isSprinting = true;
+        else if (context.canceled) isSprinting = false;
     }
 
     private void Update()
     {
-        MovePlayer();
+        HandleLook();
+        HandleMovement();
         ApplyGravity();
     }
 
-    private void MovePlayer()
+    private void HandleLook()
     {
-        float currentSpeed = isSprinting ? sprintSpeed : walkSpeed;
+        float mouseX = lookInput.x * mouseSensitivity * Time.deltaTime;
+        float mouseY = lookInput.y * mouseSensitivity * Time.deltaTime;
 
-        Vector3 move = new Vector3(inputVector.x, 0, inputVector.y);
-        move = transform.TransformDirection(move);
-        controller.Move(move * currentSpeed * Time.deltaTime);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void HandleMovement()
+    {
+        float speed = isSprinting ? sprintSpeed : walkSpeed;
+        Vector3 move = transform.right * inputVector.x + transform.forward * inputVector.y;
+        controller.Move(move * speed * Time.deltaTime);
     }
 
     private void ApplyGravity()
@@ -67,6 +81,10 @@ public class PlayerMovement : MonoBehaviour
         }
 
         velocity.y += gravity * Time.deltaTime;
+
+        // Safety catch to prevent the NaN error
+        if (float.IsNaN(velocity.y)) velocity.y = 0;
+
         controller.Move(velocity * Time.deltaTime);
     }
 }
